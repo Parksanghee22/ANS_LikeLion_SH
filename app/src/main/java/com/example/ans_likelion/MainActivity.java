f97d78cb00d8887c7bf38055ab33ea59fb8f1622
Parksanghee22 <parksh990521@gmail.com>
2020-07-17 18:09:28 +0900

    Initial commit

---
+++ app/src/main/java/com/example/ans_likelion/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.ans_likelion;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Vibrator vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(3000);
+    }
+}
